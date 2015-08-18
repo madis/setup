1a499c9409bb758e0f263e44e9b2b314aef7c508
Madis Nõmme <madis.nomme@gmail.com>
2015-08-18 21:42:29 +0300

    Use dotfiles from current folder

--- setup.sh
+++ setup.sh
@@ -3,12 +3,14 @@
 # Not needed for linux system also probably not with retina display
 defaults -currentHost write -globalDomain AppleFontSmoothing -int 0
 
+# Install some system services
 # Link bash_login to get prompt & other goodies
-ln -fs /Users/$USER/.setup/.bash_login /Users/$USER/.bash_login
-ln -fs /Users/$USER/.setup/.bashrc /Users/$USER/.bashrc
-ln -fs /Users/$USER/.setup/dotfiles/.vim /Users/$USER/.vim
-ln -fs /Users/$USER/.setup/dotfiles/.vimrc /Users/$USER/.vimrc
-ln -fs /Users/$USER/.setup/dotfiles/.gvimrc /Users/$USER/.gvimrc
+ln -fs ./dotfiles/.bash_login /Users/$USER/.bash_login
+ln -fs ./dotfiles/.bashrc /Users/$USER/.bashrc
+ln -fs ./dotfiles/.vim /Users/$USER/.vim
+ln -fs ./dotfiles/.vimrc /Users/$USER/.vimrc
+ln -fs ./dotfiles/.gvimrc /Users/$USER/.gvimrc
+
 # Configure git to use global gitignore
 ln -fs /Users/$USER/.setup/dotfiles/.gitignore /Users/$USER/.gitignore
 git config --global core.excludesfile ~/.gitignore
