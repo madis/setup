7b06c258818224bf08c5df0120022584ad0631d0
Madis Nõmme <me@mad.is>
2018-05-08 08:45:23 +0300

    Setup for python and common machine learning libraries

---
+++ setup_scripts/python.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+brew install python
+
+pip install numpy
+pip install scipy
+pip install matplotlib
+pip install ipython
+pip install jupyter
+pip install pandas
+pip install psycopg2
+pip install scikit-learn
